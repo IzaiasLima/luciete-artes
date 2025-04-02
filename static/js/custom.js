c4e37014ddf17fa72861f40cd18cfc123f85d3e8
Izaias Lima <izaiasmlima@gmail.com>
2025-04-02 20:11:52 -0300

    Ajustar altura do menu

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,10 +4,10 @@ const carousel = document.getElementById("template-mo-zay-hero-carousel")
 window.addEventListener('scroll', function() {
     const position = carousel.getBoundingClientRect();
     
-    if (position.top < -110 && position.top > -150) {
+    if (position.top < -80 && position.top > -110) {
         menu.classList.add("dark-shadow")
     }
-    if (position.top > -110) {
+    if (position.top > -80) {
         menu.classList.remove("dark-shadow")
     }
 });
