695d9909216d9d3ecb31a98404af31ed6bece860
Izaias Lima <izaiasmlima@gmail.com>
2025-04-02 22:56:22 -0300

    Ajustar o topo das paginas

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,13 @@
 const menu = document.getElementById("menu")
-const carousel = document.getElementById("template-mo-zay-hero-carousel")
+const carousel = document.getElementsByTagName("body")[0]
 
 window.addEventListener('scroll', function() {
     const position = carousel.getBoundingClientRect();
     
-    if (position.top < -80 && position.top > -110) {
+    if (position.top < -5 && position.top > -40) {
         menu.classList.add("dark-shadow")
     }
-    if (position.top > -80) {
+    if (position.top > -4) {
         menu.classList.remove("dark-shadow")
     }
 });
